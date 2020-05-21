c669eda9cff11700e218356c11756d67bebd00d2
soni-ratnesh <ratneshk.99@gmail.com>
2020-05-21 10:31:36 +0530

    Config
    Added config file for managing app.

---
+++ config.py
@@ -0,0 +1,19 @@
+"""Class-based Flask app configuration."""
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Configuration from environment variables."""
+
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'wsgi.py'
+
+    # Static Assets
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    COMPRESSOR_DEBUG = True
